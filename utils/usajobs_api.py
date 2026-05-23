16d9db5ce33e72f2f771c0c8fcfe75455f964542
productankur <ankur.product.champ@gmail.com>
2026-05-23 11:32:50 +0000

    Fix usajobs_api.py for Streamlit Cloud

--- utils/usajobs_api.py
+++ utils/usajobs_api.py
@@ -1,27 +1,28 @@
 import requests
-from utils.config import USAJOBS_API_KEY
+import os
 
-def fetch_usajobs(keyword, location = "remote", results_per_page=5):
-  headers = {
+def fetch_usajobs(keyword, location="remote", results_per_page=5):
+    # Works on both Colab and Streamlit Cloud
+    try:
+        import streamlit as st
+        USAJOBS_API_KEY = st.secrets["USAJOBS_API_KEY"]
+    except:
+        from config import USAJOBS_API_KEY
+
+    headers = {
         "Authorization-Key": USAJOBS_API_KEY,
         "User-Agent": "ankur.product.champ@gmail.com",
         "Host": "data.usajobs.gov"
-  }
-
-  params = {
+    }
+    params = {
         "Keyword": keyword,
         "LocationName": location,
         "ResultsPerPage": results_per_page
-  }
-
-  url = "https://data.usajobs.gov/api/search"
-
-  response = requests.get(url, headers=headers, params=params)
-
-  if response.status_code != 200:
+    }
+    url = "https://data.usajobs.gov/api/search"
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200:
         return []
-
-  data = response.json()
-  jobs = data.get("SearchResult", {}).get("SearchResultItems",[])
-  return jobs
-
+    data = response.json()
+    jobs = data.get("SearchResult", {}).get("SearchResultItems", [])
+    return jobs
